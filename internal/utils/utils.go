675897eb7e35d941108ecb6e2dbb4d5d2d5a8110
Ryan Sheppard <ryandsheppard95@gmail.com>
2023-08-27 17:44:37 -0400

    reorganize

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,17 +1,25 @@
 package utils
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 	"strconv"
 	"strings"
+
+	"github.com/labstack/echo/v4"
+
+	"github.com/ryansheppard/weather/internal/noaa"
 )
 
-type Coordinates struct {
+const baseurl = "https://api.weather.gov"
+
+type coordinates struct {
 	Latitude  float64 `json:"latitude"`
 	Longitude float64 `json:"longitude"`
 }
 
-func ParseCoordinates(raw string) *Coordinates {
+func parseCoordinates(raw string) *coordinates {
 	rawCoords := strings.Split(raw, ",")
 	lat, err := strconv.ParseFloat(strings.TrimSpace(rawCoords[0]), 64)
 	if err != nil {
@@ -21,10 +29,55 @@ func ParseCoordinates(raw string) *Coordinates {
 	if err != nil {
 		log.Fatal(err)
 	}
-	coords := &Coordinates{
+	coords := &coordinates{
 		Latitude:  lat,
 		Longitude: long,
 	}
 
 	return coords
+}
+
+func GetForecast(c echo.Context) error {
+	var err error
+
+	rawCoords := c.Param("coords")
+	limit := c.QueryParam("limit")
+
+	maxPeriods := 0 // Ignore if 0
+	if limit != "" {
+		maxPeriods, err = strconv.Atoi(limit)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	coords := parseCoordinates(rawCoords)
+
+	n := noaa.NewNOAA(baseurl)
+	point, err := n.GetPoints(coords.Latitude, coords.Longitude)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	forecast, err := n.GetForecast(point)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	forecasts := []string{}
+	for _, period := range forecast.Properties.Periods {
+		forecastString := fmt.Sprintf("%s: %s", period.Name, period.DetailedForecast)
+		forecasts = append(forecasts, forecastString)
+	}
+
+	if maxPeriods > 0 && len(forecasts) > maxPeriods {
+		forecasts = forecasts[:maxPeriods]
+	}
+
+	resp := echo.Map{
+		"coords":    rawCoords,
+		"forecasts": forecasts,
+	}
+
+	return c.Render(http.StatusOK, "weather.html", resp)
 }
--- main.go
+++ main.go
@@ -1,64 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"strconv"
-
 	"github.com/foolin/goview/supports/echoview-v4"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"github.com/ryansheppard/weather/internal/noaa"
 	"github.com/ryansheppard/weather/internal/utils"
 )
-
-const baseurl = "https://api.weather.gov"
-
-func getForecast(c echo.Context) error {
-	var err error
-
-	rawCoords := c.Param("coords")
-	limit := c.QueryParam("limit")
-
-	maxPeriods := 0 // Ignore if 0
-	if limit != "" {
-		maxPeriods, err = strconv.Atoi(limit)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	coords := utils.ParseCoordinates(rawCoords)
-
-	n := noaa.NewNOAA(baseurl)
-	point, err := n.GetPoints(coords.Latitude, coords.Longitude)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	forecast, err := n.GetForecast(point)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	forecasts := []string{}
-	for _, period := range forecast.Properties.Periods {
-		forecastString := fmt.Sprintf("%s: %s", period.Name, period.DetailedForecast)
-		forecasts = append(forecasts, forecastString)
-	}
-
-	if maxPeriods > 0 && len(forecasts) > maxPeriods {
-		forecasts = forecasts[:maxPeriods]
-	}
-
-	resp := echo.Map{
-		"coords":    rawCoords,
-		"forecasts": forecasts,
-	}
-
-	return c.Render(http.StatusOK, "weather.html", resp)
-}
 
 func main() {
 	e := echo.New()
@@ -70,7 +17,7 @@ func main() {
 
 	e.Renderer = echoview.Default()
 
-	e.GET("/f/:coords", getForecast)
+	e.GET("/f/:coords", utils.GetForecast)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
