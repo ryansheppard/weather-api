099b1427086d1ee0d25c2cf55f6344ac71897e13
Ryan Sheppard <ryandsheppard95@gmail.com>
2023-08-28 21:33:25 -0400

    round floats

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"math"
 	"strconv"
 	"strings"
 )
@@ -10,19 +11,31 @@ type Coordinates struct {
 	Longitude float64 `json:"longitude"`
 }
 
+// Taken from https://gosamples.dev/round-float/
+func roundFloat(val float64, precision uint) float64 {
+	ratio := math.Pow(10, float64(precision))
+	return math.Round(val*ratio) / ratio
+}
+
 func ParseCoordinates(raw string) (coords *Coordinates, err error) {
 	rawCoords := strings.Split(raw, ",")
+
 	lat, err := strconv.ParseFloat(strings.TrimSpace(rawCoords[0]), 64)
 	if err != nil {
 		return
 	}
+
 	long, err := strconv.ParseFloat(strings.TrimSpace(rawCoords[1]), 64)
 	if err != nil {
 		return
 	}
+
+	latRounded := roundFloat(lat, 3)
+	longRounded := roundFloat(long, 3)
+
 	coords = &Coordinates{
-		Latitude:  lat,
-		Longitude: long,
+		Latitude:  latRounded,
+		Longitude: longRounded,
 	}
 
 	return
--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -4,35 +4,47 @@ import "testing"
 
 func TestParseCoordinates(t *testing.T) {
 	t.Parallel()
-	want := &coordinates{
+	want := &Coordinates{
 		Latitude:  1.0,
 		Longitude: -1.0,
 	}
-	got := parseCoordinates("1.0, -1.0")
+	got, _ := ParseCoordinates("1.0, -1.0")
 	if got.Latitude != want.Latitude || got.Longitude != want.Longitude {
-		t.Errorf("parseCoordinates() = %v, want %v", got, want)
+		t.Errorf("ParseCoordinates() = %v, want %v", got, want)
+	}
+}
+
+func TestParseCoordinatesRound(t *testing.T) {
+	t.Parallel()
+	want := &Coordinates{
+		Latitude:  1.123,
+		Longitude: -1.152,
+	}
+	got, _ := ParseCoordinates("1.12345, -1.15235")
+	if got.Latitude != want.Latitude || got.Longitude != want.Longitude {
+		t.Errorf("ParseCoordinates() = %v, want %v", got, want)
 	}
 }
 
 func TestParseCoordinatesNoSpace(t *testing.T) {
 	t.Parallel()
-	want := &coordinates{
+	want := &Coordinates{
 		Latitude:  1.0,
 		Longitude: -1.0,
 	}
-	got := parseCoordinates("1.0,-1.0")
+	got, _ := ParseCoordinates("1.0,-1.0")
 	if got.Latitude != want.Latitude || got.Longitude != want.Longitude {
-		t.Errorf("parseCoordinates() = %v, want %v", got, want)
+		t.Errorf("ParseCoordinates() = %v, want %v", got, want)
 	}
 }
 func TestParseCoordinatesLotsOfSpace(t *testing.T) {
 	t.Parallel()
-	want := &coordinates{
+	want := &Coordinates{
 		Latitude:  1.0,
 		Longitude: -1.0,
 	}
-	got := parseCoordinates("  1.0 ,     -1.0")
+	got, _ := ParseCoordinates("  1.0 ,     -1.0")
 	if got.Latitude != want.Latitude || got.Longitude != want.Longitude {
-		t.Errorf("parseCoordinates() = %v, want %v", got, want)
+		t.Errorf("ParseCoordinates() = %v, want %v", got, want)
 	}
 }
